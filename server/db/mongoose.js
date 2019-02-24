cba46e981315194d3e871c85359d0aa203b88488
ofaniel <dancv@outlook.es>
2019-02-24 15:21:19 -0500

    adding thenc at mongoose.connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/YourDB", {
-  useNewUrlParser: true
-});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/YourDB", {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log("Connect to Database");
+  })
+  .catch(err => {
+    console.log("Not connected to Database ERROR ", err);
+  });
 
 export { mongoose };
