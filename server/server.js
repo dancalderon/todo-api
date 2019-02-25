a448b379a74540b1e2965a27070197b3b7e2b8b3
ofaniel <dancv@outlook.es>
2019-02-24 21:59:31 -0500

    Adding PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import { mongoose } from "./db/mongoose";
 import { Todo } from "./models/todo";
 import { User } from "./models/user";
 import { ObjectID } from "mongodb";
+import _ from "lodash";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -48,6 +49,33 @@ app.delete("/todos/:id", (req, res) => {
       !todo ? res.status(404).send() : res.status(200).send({ todo })
     )
     .catch(err => res.status(400).send(`404 not found`));
+});
+
+app.patch("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  //pick will take an object and will take the properties that we passed, if they exist
+  //this returns an object
+  const body = _.pick(req.body, ["text", "completed"]);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  //type of 'boolean' and completed = true
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  //fins the item by id, declares what update will be make it and options
+  //$set will take the body object, and set the todo values as the body values
+  // in this case todo.completed = body.complited, and todo.text = body.text
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) return res.status(404).send();
+      res.send({ todo });
+    })
+    .catch(err => res.status(400).send());
 });
 
 app.listen(port, () => {
